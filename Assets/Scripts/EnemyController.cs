122768a0c78da78e60023c7b80ac8afad0655ec6
Tatyumi <s150204@nishitech.ac.jp>
2019-03-14 22:33:57 +0900

    敵キャラを表す列挙型の追加
    ゲームオーバシーンを表示する際に，衝突した敵キャラによって演出を変えるため，判別のために追加した．

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,8 +2,19 @@
 
 public class EnemyController : MonoBehaviour
 {
+    /// <summary>プレイヤーに当たったエネミー</summary>
+    public static int AttackEnemy = 0;
     /// <summary>左側画面外のX座標</summary>
     private readonly int offScreenPositionX = (Screen.width / 2) * -1;
+
+    // 敵キャラのカテゴリ
+    public enum EnemyCategory
+    {
+        None,   // 該当なし
+        Kurage, // クラゲ
+        Konbu,  // コンブ
+        Net     // ネット
+    }
 
     /// <summary>
     /// オブジェクトが画面外かどうか
