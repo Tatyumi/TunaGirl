07951027d22e02c73375ef0eb33c5d4443a2b7f0
Tatyumi <s150204@nishitech.ac.jp>
2019-03-15 01:51:41 +0900

    移動制限処理を追加
    画面外にプレイヤーが移動しないよう，行動制限処理を追加した．

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     private const float moveSpeed = 7.0f;
     /// <summary>ゲームディレクター</summary>
     private GameDirector gameDirector;
+    /// <summary>移動できるY座標の最大値</summary>
+    private float maxMovePositionY;
+    /// <summary>移動できるY座標の最小値</summary>
+    private float minMovePositionY;
 
     private void Awake()
     {
@@ -17,10 +21,12 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-
         // 初期位置
-        this.transform.localPosition =
-            new Vector2((Screen.width / 2) * -1 + this.gameObject.GetComponent<RectTransform>().sizeDelta.x / 2, 0);
+        this.transform.localPosition = new Vector2((Screen.width / 2) * -1
+            + this.gameObject.GetComponent<RectTransform>().sizeDelta.x / 2, 0);
+
+        maxMovePositionY = Screen.height / 2 - this.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
+        minMovePositionY = maxMovePositionY * -1;
     }
 
     void Update()
@@ -37,6 +43,16 @@ public class PlayerController : MonoBehaviour
         {
             // プレイヤーを下に移動
             this.transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed);
+        }
+
+        // 画面外に移動してないか判別
+        if(this.transform.localPosition.y > maxMovePositionY)
+        {
+            this.transform.localPosition = new Vector2(transform.localPosition.x, maxMovePositionY);
+        }
+        else if(this.transform.localPosition.y < minMovePositionY)
+        {
+            this.transform.localPosition = new Vector2(transform.localPosition.x, minMovePositionY);
         }
     }
 
