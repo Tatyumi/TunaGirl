36bffb8c8a8bde4a4d6c1749a23620b469913a34
Tatyumi <s150204@nishitech.ac.jp>
2019-03-14 11:04:52 +0900

    新規追加
    共通するもし列や定数を一元管理するスクリプトを追加．

---
+++ Assets/Scripts/Common.cs
@@ -0,0 +1,17 @@
+﻿
+// 共通するものを一元管理
+namespace Common
+{
+    // シーン名
+    public class SceneName
+    {
+        /// <summary>タイトルシーン名</summary>
+        public const string TITLE_SCENE = "TitleScene";
+        /// <summary>ゲームシーン名</summary>
+        public const string GAME_SCENE= "GameScene";
+        /// <summary>ゲームクリアシーン名</summary>
+        public const string GAME_CLEAR_SCENE= "GameClear";
+        /// <summary>ゲームオーバーシーン名</summary>
+        public const string GAME_OVER_SCENE= "GameOverScene";
+    }
+}
