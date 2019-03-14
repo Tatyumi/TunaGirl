2278176675373bd17b71612a647af88b14b2f28b
Tatyumi <s150204@nishitech.ac.jp>
2019-03-14 12:16:41 +0900

    タイトルシーンの演出を追加
    画面の構成，演出，また，クリックするとシーン遷移等の機能を追加した．

---
+++ Assets/Scripts/TitleDirector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+public class TitleDirector : MonoBehaviour
+{
+    void Update()
+    {
+        // 画面がタップされた場合
+        if (Input.GetMouseButtonDown(0))
+        {
+            // ゲームシーンに移動
+            SceneManager.LoadScene(SceneName.GAME_SCENE);
+        }
+    }
+}
