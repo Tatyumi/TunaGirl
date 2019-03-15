e3f0da407d773529cba3d36f37415fed0e12f23b
Tatyumi <s150204@nishitech.ac.jp>
2019-03-15 19:07:12 +0900

    新規追加
    音の制御を行うスクリプトを追加した．
    また，シングルトンで行うため，それが実現できるスクリプトも追加した．

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+    /// <summary>タイトルシーンBGM</summary>
+    public AudioClip TitleSceneBGM;
+    /// <summary>ゲームシーンBGM</summary>
+    public AudioClip GameSceneBGM;
+    /// <summary>ゲームオーバーシーンBGM</summary>
+    public AudioClip GameOverSceneBGM;
+    /// <summary>ゲームクリアシーンBGM</summary>
+    public AudioClip GameClearSceneBGM;
+
+    /// <summary>オーディオソース</summary>
+    AudioSource audioSource;
+    /// <summary>全オーディオ保持ディクショナリ</summary>
+    private Dictionary<string, AudioClip> AudioDic;
+
+    private void Awake()
+    {
+        // 存在確認
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        //シーンが遷移しても破棄されない
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        // オーディオを格納
+        AudioDic = new Dictionary<string, AudioClip> {
+            { TitleSceneBGM.name, TitleSceneBGM},
+            { GameSceneBGM.name, GameSceneBGM},
+            { GameOverSceneBGM.name, GameOverSceneBGM},
+            { GameClearSceneBGM.name, GameClearSceneBGM},
+        };
+    }
+
+    /// <summary>
+    /// 音を流す
+    /// </summary>
+    /// <param name="soundName">音の名前</param>
+    public void PlaySound(string soundName)
+    {
+        // 名前のチェック
+        if (!AudioDic.ContainsKey(soundName))
+        {
+            Debug.Log(soundName + "という名前のSEがありません");
+            return;
+        }
+        audioSource.PlayOneShot(AudioDic[soundName]);
+    }
+
+    /// <summary>
+    /// 音を停止
+    /// </summary>
+    public void StopSound()
+    {
+        audioSource.Stop();
+    }
+
+}
---
+++ Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// シングルトンの実装
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            // 存在チェック
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null)
+                {
+                    Debug.LogError(typeof(T) + "is nothing");
+                }
+            }
+
+            return instance;
+        }
+    }
+}
