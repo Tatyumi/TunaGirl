8358c3001e00f4af836aa28573f971154df8d52c
Tatyumi <s150204@nishitech.ac.jp>
2019-03-16 11:58:28 +0900

    新規追加
    敵キャラ「オクトパス」を追加した．
    それに伴い，アニメーション，効果音，じゃねレーション等の追加を行った．

---
+++ Assets/Scripts/OctopusGenerator.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using Common;
+
+public class OctopusGenerator : MonoBehaviour
+{
+    /// <summary>ゲームディレクターオブジェクト</summary>
+    public GameObject GameDirector;
+    /// <summary>プレイヤーオブジェクト</summary>
+    public GameObject Player;
+    /// <summary>キャンバス</summary>
+    public GameObject Canvas;
+    /// <summary>オクトパスプレファブ </summary>
+    public GameObject OctpusPrefab;
+    /// <summary>出現間隔</summary>
+    private float span = 8.0f;
+    /// <summary></summary>
+    private float delta = 0.0f;
+    /// <summary>オクトパスプレファブ生成時のX座標</summary>
+    private float octpusPositionX;
+    /// <summary>ゲームディレクター</summary>
+    private GameDirector gameDirector;
+
+    void Awake()
+    {
+        gameDirector = GameDirector.GetComponent<GameDirector>();
+    }
+
+    void Start()
+    {
+        // 画面右端の座標を取得
+        octpusPositionX = Screen.width / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+
+        // 一定時間たったか判別
+        if (this.delta > span)
+        {
+            this.delta = 0.0f;
+            GameObject gameObject = Instantiate(OctpusPrefab) as GameObject;
+
+            // 生成するオブジェクトをCanVasの子にする
+            gameObject.transform.SetParent(Canvas.transform, false);
+
+
+            // プレイヤーのY座標に合わせてプレファブを生成
+            gameObject.transform.localPosition = new Vector2(octpusPositionX, Player.transform.localPosition.y);
+
+            // オクトパス生成時のSeを再生
+            gameDirector.audioManager.PlaySound(AudioName.OCTPUS_SE);
+        }
+    }
+}
---
+++ Assets/Scripts/OctpusController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+public class OctpusController : EnemyController, IKillablePlayer
+{
+    /// <summary>移動速度</summary>
+    private float moveSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //少しずつ左に移動
+        this.transform.position = new Vector3(transform.position.x - (Mathf.PingPong(Time.time, 1) * moveSpeed), transform.position.y);
+
+        // 画面外か判別
+        base.CheckOffScreen(this.transform.localPosition.x);
+
+    }
+
+    /// <summary>
+    /// ゲーム終了処理
+    /// </summary>
+    public void KillPlayer()
+    {
+        base.DetectAttackEnemy((int)EnemyCategory.Octopus);
+        SceneManager.LoadScene(SceneName.GAME_OVER_SCENE);
+    }
+}
