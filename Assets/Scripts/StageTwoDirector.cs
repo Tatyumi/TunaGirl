6e3ddd718ec903c7ea7abfa4ab6476fe6667eaca
Tatyumi <s150204@nishitech.ac.jp>
2019-05-22 15:08:54 +0900

    新規追加

---
+++ Assets/Scripts/StageTwoDirector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+public class StageTwoDirector : MonoBehaviour
+{
+
+    /// <summary>オーディオソース</summary>
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        // オーディオマネージャー取得
+        audioManager = AudioManager.Instance;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // BGMの再生
+        audioManager.PlaySound(AudioName.STAGE_TWO_SCENE_BGM);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
